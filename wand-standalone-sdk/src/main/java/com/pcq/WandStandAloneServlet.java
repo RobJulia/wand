b5595c322e639e414085f88434ffea15447a48de
pcq <pengchengqing@mobike.com>
2017-07-17 16:59:28 +0800

    修改代码

--- wand-standalone-sdk/src/main/java/com/pcq/WandStandAloneServlet.java
+++ wand-standalone-sdk/src/main/java/com/pcq/WandStandAloneServlet.java
@@ -3,6 +3,7 @@ package com.pcq;
 import com.alibaba.fastjson.JSONObject;
 import com.pcq.annotation.WandMethod;
 import com.pcq.utils.StringUtils;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
@@ -15,9 +16,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import com.pcq.WMethod;
-import com.pcq.WParameter;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -26,55 +26,77 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class WandStandAloneServlet
         extends HttpServlet
-        implements ApplicationContextAware
-{
+        implements ApplicationContextAware {
+    private String secretKey = "";
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
     Map<String, Object> beanMap = new HashMap();
     Map<String, WMethod> wandMethodMap = new HashMap();
     private static ApplicationContext applicationContext = null;
 
-    public static ApplicationContext getApplicationContext()
-    {
+    public static ApplicationContext getApplicationContext() {
         return applicationContext;
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         String requestURI = req.getRequestURI();
         PrintWriter printWriter = resp.getWriter();
+        if (StringUtils.isEmpty(secretKey)) {
+            printWriter.println("未配置访问秘钥");
+            return;
+        }
+
+        HttpSession session = req.getSession();
+
+        String secretKeyStr = (String) req.getParameter("secretKey");
+        //先从参数中获取
+        //有的话继续访问，并放到session中
+        //否则 查询session 如果都没有则没有权限
+        if (StringUtils.isEmpty(secretKeyStr)) {
+            secretKeyStr = (String) session.getAttribute("secretKey");
+        }else{
+            session.setAttribute("secretKey", secretKey);
+            session.setMaxInactiveInterval(20 * 60);
+        }
+        if (!secretKey.equals(secretKeyStr)) {
+            printWriter.println("无权限");
+            return;
+        }
         printWriter.println("<html>");
         printWriter.println("<head> ");
         printWriter.println("<h1>wand </h1><body>");
         String methodName;
-        if (requestURI.startsWith("/wand/methodList"))
-        {
+        if (requestURI.startsWith("/wand/methodList")) {
             printWriter.println("<table><tr><th>服务</th><th>方法</th><th>方法描述</th></tr><p>");
-            for (String wMethodName : this.wandMethodMap.keySet())
-            {
+            for (String wMethodName : this.wandMethodMap.keySet()) {
                 String[] key = wMethodName.split("#");
                 String className = key[0];
                 methodName = key[1];
                 printWriter.println("<tr><td>" + className + "</td>" + "<td>" + methodName + "</td>" + "<td>" +
 
-                        ((WMethod)this.wandMethodMap.get(wMethodName)).getMethodDesc() + "</td>" + "<td><a href=\"/wand/methodInfo?className=" + className + "&methodName=" + methodName + "\">调用</a></td></tr>");
+                        ((WMethod) this.wandMethodMap.get(wMethodName)).getMethodDesc() + "</td>" + "<td><a href=\"/wand/methodInfo?className=" + className + "&methodName=" + methodName + "\">调用</a></td></tr>");
             }
             printWriter.println("</table>");
-        }
-        else if (requestURI.startsWith("/wand/methodInfo"))
-        {
+        } else if (requestURI.startsWith("/wand/methodInfo")) {
             String className = req.getParameter("className");
             methodName = req.getParameter("methodName");
-            WMethod wMethod = (WMethod)this.wandMethodMap.get(className + "#" + methodName);
+            WMethod wMethod = (WMethod) this.wandMethodMap.get(className + "#" + methodName);
             printWriter.println("服务名：" + className + "<p>");
             printWriter.println("方法名：" + methodName + "<p>");
             printWriter.println("<form action=\"/wand/invoke\" method=\"POST\"><table>");
             printWriter.println("<tr><th>类型</th><th>参数名</th><th>参数值</th></tr>");
             printWriter.println("<input name=\"className\" hidden value=\"" + className + "\"></input>");
             printWriter.println("<input name=\"methodName\" hidden value=\"" + methodName + "\"></input>");
-
             int i = 0;
-            for (WParameter parameter : wMethod.getParameters())
-            {
+            for (WParameter parameter : wMethod.getParameters()) {
                 String name = parameter.getName() == null ? "" : parameter.getName();
                 printWriter.println("<tr><td>" + parameter
                         .getType() + "</td>" + "<td>" + name + "</td>" + "<td><input name=\"field-" + i + "\"></input></td>" + "</tr>");
@@ -89,101 +111,91 @@ public class WandStandAloneServlet
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         String requestURI = req.getRequestURI();
-        if (requestURI.startsWith("/wand/invoke"))
-        {
+        HttpSession session = req.getSession();
+        String secretKeyStr = (String) session.getAttribute("secretKey");
+        PrintWriter printWriter = resp.getWriter();
+
+        if (!secretKey.equals(secretKeyStr)) {
+            printWriter.println("无权限");
+            return;
+        }
+        if (requestURI.startsWith("/wand/invoke")) {
+
             String className = req.getParameter("className");
             String methodName = req.getParameter("methodName");
             Map<String, String[]> parameterMap = req.getParameterMap();
             Object bean = this.beanMap.get(className);
-            WMethod wMethod = (WMethod)this.wandMethodMap.get(className + "#" + methodName);
+            WMethod wMethod = (WMethod) this.wandMethodMap.get(className + "#" + methodName);
             List<WParameter> wParameterList = wMethod.getParameters();
 
             int count = wParameterList.size();
             String[] paramArr = new String[count];
-            for (Map.Entry<String, String[]> entry : parameterMap.entrySet())
-            {
-                String key = (String)entry.getKey();
+            for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+                String key = (String) entry.getKey();
                 String[] split = key.split("-");
-                if (split.length > 1)
-                {
+                if (split.length > 1) {
                     int seq = Integer.parseInt(split[1]);
-                    paramArr[seq] = ((String[])entry.getValue())[0];
+                    paramArr[seq] = ((String[]) entry.getValue())[0];
                 }
             }
             Object[] parameters = new Object[count];
-            PrintWriter printWriter = resp.getWriter();
-            for (int i = 0; i < count; i++)
-            {
-                WParameter wParameter = (WParameter)wParameterList.get(i);
+            for (int i = 0; i < count; i++) {
+                WParameter wParameter = (WParameter) wParameterList.get(i);
                 BuildParameterRes buildParameterRes = buildParameter(paramArr[i], wParameter.getType());
-                if (!buildParameterRes.isSuccess())
-                {
+                if (!buildParameterRes.isSuccess()) {
                     printWriter.println("第" + (i + 1) + "参数非法");
                     return;
                 }
                 parameters[i] = buildParameterRes.getData();
             }
             Method method = wMethod.getMethod();
-            try
-            {
+            try {
                 Object res = method.invoke(bean, parameters);
                 printWriter.println(JSONObject.toJSONString(res));
-            }
-            catch (IllegalAccessException e)
-            {
+            } catch (IllegalAccessException e) {
                 e.printStackTrace();
-            }
-            catch (InvocationTargetException e)
-            {
+            } catch (InvocationTargetException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private BuildParameterRes buildParameter(String o, String type)
-    {
-        try
-        {
+    private BuildParameterRes buildParameter(String o, String type) {
+        try {
             if (type.equals("java.lang.String")) {
                 return new BuildParameterRes(true, o);
             }
-            if (type.equals("int"))
-            {
+            if (type.equals("int")) {
                 if (StringUtils.isEmpty(o)) {
                     return new BuildParameterRes(true, Integer.valueOf(0));
                 }
                 int a = Integer.valueOf(o).intValue();
                 return new BuildParameterRes(true, Integer.valueOf(a));
             }
-            if (type.equals("long"))
-            {
+            if (type.equals("long")) {
                 if (StringUtils.isEmpty(o)) {
                     return new BuildParameterRes(true, Long.valueOf(0L));
                 }
                 long a = Long.valueOf(o).longValue();
                 return new BuildParameterRes(true, Long.valueOf(a));
             }
-            if (type.equals("float"))
-            {
+            if (type.equals("float")) {
                 if (StringUtils.isEmpty(o)) {
                     return new BuildParameterRes(true, Float.valueOf(0.0F));
                 }
                 float a = Float.valueOf(o).floatValue();
                 return new BuildParameterRes(true, Float.valueOf(a));
             }
-            if (type.equals("double"))
-            {
+            if (type.equals("double")) {
                 if (StringUtils.isEmpty(o)) {
                     return new BuildParameterRes(true, Float.valueOf(0.0F));
                 }
                 double a = Double.valueOf(o).doubleValue();
                 return new BuildParameterRes(true, Double.valueOf(a));
             }
-            if (type.equals("boolean"))
-            {
+            if (type.equals("boolean")) {
                 if (StringUtils.isEmpty(o)) {
                     return new BuildParameterRes(true, Boolean.valueOf(false));
                 }
@@ -201,8 +213,7 @@ public class WandStandAloneServlet
             if (type.equals("java.lang.Long")) {
                 return new BuildParameterRes(true, Long.valueOf(o));
             }
-            if (type.equals("java.lang.Boolean"))
-            {
+            if (type.equals("java.lang.Boolean")) {
                 if (o.equalsIgnoreCase("true")) {
                     return new BuildParameterRes(true, Boolean.TRUE);
                 }
@@ -214,40 +225,30 @@ public class WandStandAloneServlet
             if (type.equals("java.lang.Double")) {
                 return new BuildParameterRes(true, Double.valueOf(o));
             }
-        }
-        catch (NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             return BuildParameterRes.buildFailedRes(false, "参数类型错误");
         }
         return BuildParameterRes.buildFailedRes(false, "参数类型错误");
     }
 
-    public void init()
-    {
+    public void init() {
         ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
         String[] beanNames = applicationContext.getBeanDefinitionNames();
-        for (String beanName : beanNames)
-        {
+        for (String beanName : beanNames) {
             boolean isPrototype = applicationContext.isPrototype(beanName);
-            if (!isPrototype)
-            {
+            if (!isPrototype) {
                 Object bean = applicationContext.getBean(beanName);
-                if (bean == null)
-                {
+                if (bean == null) {
                     System.out.println("wandServlet init: " + beanName + "is null");
-                }
-                else
-                {
+                } else {
                     Class clazz = bean.getClass();
                     if (AopUtils.isAopProxy(bean)) {
                         clazz = AopUtils.getTargetClass(bean);
                     }
                     Method[] methods = clazz.getMethods();
-                    for (Method method : methods)
-                    {
-                        WandMethod annotation = (WandMethod)method.getAnnotation(WandMethod.class);
-                        if (annotation != null)
-                        {
+                    for (Method method : methods) {
+                        WandMethod annotation = (WandMethod) method.getAnnotation(WandMethod.class);
+                        if (annotation != null) {
                             String clazzName = clazz.getName();
                             if (null == this.beanMap.get(clazzName)) {
                                 this.beanMap.put(clazzName, bean);
@@ -258,10 +259,10 @@ public class WandStandAloneServlet
                 }
             }
         }
+        this.secretKey = this.getInitParameter("secretKey");
     }
 
-    private WMethod builldWandMethod(Method method, String className, WandMethod anotation)
-    {
+    private WMethod builldWandMethod(Method method, String className, WandMethod anotation) {
         WMethod wMethod = new WMethod();
         wMethod.setClassName(className);
         wMethod.setMethodName(method.getName());
@@ -273,8 +274,7 @@ public class WandStandAloneServlet
         Class[] parameterTypes = method.getParameterTypes();
         String[] paramNames = params.split("&");
         int parameterSeq = 0;
-        for (Class parameterClass : parameterTypes)
-        {
+        for (Class parameterClass : parameterTypes) {
             WParameter wParameter = new WParameter();
             wParameter.setType(parameterClass.getName());
             if (paramNames.length > parameterSeq) {
@@ -289,8 +289,7 @@ public class WandStandAloneServlet
     }
 
     public void setApplicationContext(ApplicationContext applicationContext)
-            throws BeansException
-    {
+            throws BeansException {
         applicationContext = applicationContext;
     }
 }
